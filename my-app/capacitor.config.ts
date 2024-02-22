b8b2274ab1dcce7abd2e62db712905ada966809b
COLLINS TONUI <tckiprotich@outlook.com>
2024-02-22 09:49:46 +0000

    Remove unnecessary files and update README

---
+++ my-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  "appId": "com.example.app",
+  "appName": "my-app",
+  "webDir": "out",
+  "bundledWebRuntime": false
+};
+
+export default config;
